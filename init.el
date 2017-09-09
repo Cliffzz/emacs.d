03ebeffaae2f158561480d4890c436b2b15a0476
Cliffz <Cliffzz@users.noreply.github.com>
2017-09-09 18:29:42 +0200

    feat(theme): switch to gruvbox theme

--- init.el
+++ init.el
@@ -228,8 +228,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light)
+   dotspacemacs-themes '(gruvbox)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
@@ -402,6 +401,9 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  ;; Fix loading dependencies for gruvbox theme.
+  (load-file "~/.emacs.d/elpa/dash-20170810.137/dash.elc")
+  (load-file "~/.emacs.d/elpa/autothemer-20170112.1324/autothemer.elc")
   )
 
 (defun dotspacemacs/user-config ()
@@ -438,6 +440,37 @@ you should place your code here."
   ;; Eshell visual commands.
   (add-hook 'eshell-mode-hook '(lambda()
                (add-to-list 'eshell-visual-commands "htop")))
+  ;; Whitespace-mode theme.
+  (set-face-attribute 'trailing-whitespace nil :background "#7c6f64")
+  ;; Flyspell theme.
+  (set-face-attribute 'flyspell-duplicate nil :underline '(:style line :color "#fe8019"))
+  (set-face-attribute 'flyspell-incorrect nil :underline '(:style line :color "#fe8019"))
+  ;; Flycheck theme.
+  (set-face-attribute 'flycheck-fringe-error nil :foreground "#fb4933")
+  (set-face-attribute 'flycheck-fringe-warning nil :foreground "#fabd2f")
+  (set-face-attribute 'flycheck-fringe-info nil :foreground "#83a598")
+  (set-face-attribute 'flycheck-error nil :underline '(:style line :color "#fb4933"))
+  (set-face-attribute 'flycheck-warning nil :underline '(:style line :color "#fabd2f"))
+  (set-face-attribute 'flycheck-info nil :underline '(:style line :color "#83a598"))
+  ;; Spaceline theme.
+  (set-face-attribute 'powerline-active1 nil :background "#665c54")
+  (set-face-attribute 'powerline-active2 nil :background "#504945")
+  (set-face-attribute 'mode-line nil :background "#3c3836")
+  (set-face-attribute 'powerline-inactive1 nil :background "#665c54")
+  (set-face-attribute 'powerline-inactive2 nil :background "#504945")
+  (set-face-attribute 'mode-line-inactive nil :background "#3c3836")
+  (set-face-attribute 'spaceline-flycheck-error nil :foreground "#fb4933")
+  (set-face-attribute 'spaceline-flycheck-info nil :foreground "#fabd2f")
+  (set-face-attribute 'spaceline-flycheck-warning nil :foreground "#83a598")
+  ;; Diff-hl theme.
+  (set-face-attribute 'diff-hl-change nil :foreground "#83a598" :background nil)
+  (set-face-attribute 'diff-hl-delete nil :foreground "#fb4933")
+  (set-face-attribute 'diff-hl-insert nil :foreground "#b8bb26")
+  ;; Js2 theme.
+  (add-hook 'js2-mode-hook '(lambda()
+                              (set-face-attribute 'js2-error nil :underline '(:style line :color "#fb4933"))
+                              (set-face-attribute 'js2-warning nil :underline '(:style line :color "#fabd2f"))
+                              (set-face-attribute 'js2-external-variable nil :underline '(:style line :color "#b8bb26"))))
   )
 
 ;; Custom variables file.
