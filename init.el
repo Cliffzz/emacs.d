58f493df13f9bef9336e6957d0c1515ab5788816
Cliffz <Cliffzz@users.noreply.github.com>
2017-09-12 17:09:58 +0200

    perf: remove eshell ansi color filter

--- init.el
+++ init.el
@@ -337,8 +337,6 @@ you should place your code here."
   ;; Spell check camel case.
   (setq ispell-program-name "aspell")
   (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together" "--run-together-limit=5" "--run-together-min=2"))
-  ;; Output colors in eshell.
-  (add-hook 'eshell-preoutput-filter-functions 'ansi-color-apply)
   ;; Eshell visual commands.
   (add-hook 'eshell-mode-hook '(lambda()
                (add-to-list 'eshell-visual-commands "htop")))
