5baed222ba1c351f80b4ea8007455cd256daeb09
Cliffz <Cliffzz@users.noreply.github.com>
2017-10-26 15:47:01 +0200

    feat: add yaml layer

--- init.el
+++ init.el
@@ -57,6 +57,7 @@ This function should only modify configuration layer settings."
      (version-control :variables
                       version-control-diff-tool 'diff-hl)
      windows-scripts
+     yaml
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
