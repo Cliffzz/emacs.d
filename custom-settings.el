0bdee7b4e63a08badf536d77b092d9dfa59dddd9
Cliffz <Cliffzz@users.noreply.github.com>
2018-01-27 15:52:43 +0100

    feat: add emacs configuration

---
+++ custom-settings.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode dockerfile-mode markdown-mode graphql-mode pug-mode web-mode coffee-mode tide js2-mode json-mode yasnippet dumb-jump undo-tree diff-hl magit xterm-color eshell-prompt-extras counsel ivy smart-mode-line projectile rainbow-delimiters smartparens company which-key flycheck exec-path-from-shell gruvbox-theme elpa-mirror delight use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
