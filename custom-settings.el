60c2138e377e6ecc6feb7e3f4c1b7ac664a8e736
Cliffz <Cliffzz@users.noreply.github.com>
2018-02-25 15:30:08 +0100

    chore: update elpa dependencies

--- custom-settings.el
+++ custom-settings.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (es-mode yasnippet yaml-mode xterm-color which-key web-mode use-package undo-tree tide smartparens smart-mode-line rainbow-delimiters pug-mode projectile markdown-mode magit json-mode js2-mode gruvbox-theme graphql-mode exec-path-from-shell eshell-prompt-extras elpa-mirror dumb-jump dockerfile-mode diff-hl delight counsel company coffee-mode))))
+    (es-mode yaml-mode dockerfile-mode markdown-mode graphql-mode pug-mode web-mode coffee-mode tide typescript-mode js2-mode json-mode yasnippet dumb-jump undo-tree diff-hl magit xterm-color eshell-prompt-extras counsel ivy smart-mode-line projectile rainbow-delimiters smartparens company which-key flycheck exec-path-from-shell gruvbox-theme elpa-mirror delight use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
