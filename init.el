3439f25eec6d117388124e1ad55467ed166c8a5d
Cliffz <Cliffzz@users.noreply.github.com>
2018-05-29 21:27:35 +0200

    chore: update elpa and node dependencies

--- init.el
+++ init.el
@@ -165,7 +165,7 @@
 
 ;; Initialize load-path for packages.
 (setq load-path (eval-when-compile (append load-path (directory-files "~/.emacs.d/elpa" t "^[^.]" t))))
-(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/gruvbox-theme-20180313.1451")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/gruvbox-theme-20180430.506")
 
 ;; Initialize package.el only at compile time.
 (eval-when-compile
