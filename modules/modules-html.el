1accab01f6c820308cd9fa592fe3214ac463e06b
Cliffz <Cliffzz@users.noreply.github.com>
2020-01-11 18:11:55 +0100

    feat(modules-html): add code formatting

--- modules/modules-html.el
+++ modules/modules-html.el
@@ -15,6 +15,18 @@
   (defvar web-mode-markup-indent-offset)
   (setq web-mode-markup-indent-offset 4))
 
+;; Prettier, code formatting.
+(use-package prettier-js
+  :commands (prettier-js)
+  :init
+  (declare-function set-prettier-html-config "modules-html")
+  (defun set-prettier-html-config ()
+    (make-local-variable 'prettier-js-args)
+    (setq prettier-js-args '("--print-width" "100" "--tab-width" "4" "--single-quote" "--trailing-comma" "all"))
+    (bind-key "C-c f" 'prettier-js web-mode-map))
+
+  (set-html-mode-hook #'set-prettier-html-config))
+
 ;; Enable langauge server completion.
 (defun setup-html-completion ()
   (defvar company-backends)
